80f4a52a469204a79055de38f525987193e36823
sara altaee <sara.korea96@gmail.com>
2020-05-12 00:14:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
+<<<<<<< HEAD
 const companyName = 'Scuber';
 let mostProfitableNeighborhood='Chelsea';
 let companyCeo='Susan Smith';
+=======
+let companyName = 'Scuber';
+>>>>>>> 0df1bd99baaba724ee325a36c662f2655c4e537a
